eb4554e862a815e26b6a1ac62ace19447d7ecdff
trungrtr <daoquangtrung2108@gmail.com>
2026-04-16 22:21:58 +0700

    add library

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express') //nap thu vien
+const morgan = require('morgan')
 const app = express() //cha ve 1 doi tuong 
 const port = 3000 // chay web o cong nao
 
 // định nghĩa tuyến đường route(/....)
-app.get('/trangchu', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello Worldddd!')
 })
-
+app.use(morgan('combined'))
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
