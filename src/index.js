b74634d1d63f2dc10f3c28162c5a05554b35aaae
trungrtr <daoquangtrung2108@gmail.com>
2026-04-21 22:05:09 +0700

    Handlebars

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const path = require('path') // Đã sửa: bỏ ngoặc nhọn
+const express = require('express') // nap thu vien
+const morgan = require('morgan')
+const { engine } = require('express-handlebars')
+const app = express() // cha ve 1 doi tuong
+const port = 3000 // chay web o cong nao
+
+// dinh nghia tuyen duong route(/....)
+
+app.use(morgan('combined'))
+
+// template engine
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+
+// Đã sửa: dùng path.join (dấu chấm) và trỏ đúng đường dẫn thư mục views
+app.set('views', path.join(__dirname, 'resources/views'))
+
+// Route chính để render giao diện
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+// Đã comment lại đoạn này để không bị trùng với route '/' ở ngay phía trên
+// app.get('/', (req, res) => {
+//   res.send('<h1 style =red >Hello World!</h1>')
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
